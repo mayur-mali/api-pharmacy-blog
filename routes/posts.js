bde13f554afa8a37b6a9efd8c4fc4d688c8ebfbe
vampire-mayu <mayurmali31@gmail.com>
2022-06-06 09:20:43 +0530

    WIP

--- routes/posts.js
+++ routes/posts.js
@@ -15,12 +15,63 @@ router.post("/", async (req, res) => {
 });
 
 //UPDATE POST
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        return res.status(200).json(updatedPost);
+      } catch (err) {
+        return res.status(401).json(err);
+      }
+    } else {
+      return res.status(401).json("you can only update your post");
+    }
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+});
 
 //DELETE POST
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        return res.status(200).json("post has been deleted..");
+      } catch (err) {
+        return res.status(401).json(err);
+      }
+    } else {
+      return res.status(401).json("you can only delete your post");
+    }
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+});
 
 //GET POST
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  if (req.body.postsId === req.params.id) {
+    try {
+      const post = await Post.findById(req.params.id);
+      return res.status(200).json(post);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(401).json("post not found");
+  }
+});
+
+// GET ALL POST
 
 module.exports = router;
